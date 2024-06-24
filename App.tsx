5501b242aa5fee91ad175180643cdd2c94141b01
mstaples95 <141524898+mstaples95@users.noreply.github.com>
2024-06-24 12:00:05 -0400

    footer

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path='/' element = {<HomePage/>}/>
           </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import logo from '../../assets/images/logo.png'
+import { Link } from "react-router-dom";
+import GithubLogo from "../../assets/icons/icons8-github.svg"
+import LinkedInLogo from "../../assets/icons/icons8-linkedin.svg"
+import InstagramLogo from "../../assets/icons/icons8-instagram.svg"
+import TwitterLogo from "../../assets/icons/icons8-twitter.svg"
+import './footer.scss';
 
 const Footer: React.FC = () => {
     return (
-        <footer>
-            
+        <footer className="footer">
+            <div className='footer__image-container'>
+                <img className='footer__logo' src={logo} alt='logo'/>
+            </div>
+            <div className="footer__social-link-container">
+                <ul className="footer__social-link-list">
+                    <li className="footer__social-list-item">
+                        <Link to ="/"> <img className="footer__social-img" src={GithubLogo} alt="Github Link"/> </Link>
+                    </li>
+                    <li className="footer__social-list-item">
+                        <Link to ="/"> <img className="footer__social-img" src={LinkedInLogo} alt="Github Link"/> </Link>
+                    </li>
+                    <li className="footer__social-list-item">
+                        <Link to ="/"> <img className="footer__social-img" src={InstagramLogo} alt="Github Link"/> </Link>
+                    </li>
+                    <li className="footer__social-list-item">
+                        <Link to ="/"> <img className="footer__social-img" src={TwitterLogo} alt="Github Link"/> </Link>
+                    </li>
+                </ul>
+            </div>
         </footer>
     )
 }
